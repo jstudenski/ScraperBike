c20565701c711d0ccac3c82f24a35490760a39a9
jstudenski <jstudenski@gmail.com>
2018-04-02 19:58:17 -0500

    added buttons

---
+++ public/assets/js/app.js
@@ -0,0 +1,28 @@
+$(document).on("click", "#home", function() {
+
+
+  console.log('hello HOME');
+  
+});
+
+$(document).on("click", "#scrape", function() {
+
+
+  console.log('hello SCRAPE');
+
+});
+
+$(document).on("click", "#saved", function() {
+
+
+  console.log('hello SAVED');
+  
+});
+
+
+
+
+
+
+
+
