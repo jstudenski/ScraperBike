41deb1d198bc36605e0e31d7e5079b06e2829871
jstudenski <jstudenski@gmail.com>
2018-04-06 23:56:56 -0500

    light clean up

--- models/Headline.js
+++ models/Headline.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-// Save a reference to the Schema constructor
+
 const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 const HeadlineSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     trim: true,
@@ -36,16 +33,12 @@ const HeadlineSchema = new Schema({
   imageURL: {
     type: String
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 const Headline = mongoose.model("Headline", HeadlineSchema);
 
 module.exports = Headline;
